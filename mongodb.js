b49161fa3673f10fe0d1c5cc2d5729bde717f190
amitkumar18596 <maharana.amit96@gmail.com>
2022-07-25 23:54:39 +0530

    studied hoe to findone/many, update one/many, delete one/many

--- mongodb.js
+++ mongodb.js
@@ -4,13 +4,13 @@
 // const MongoClient = mongodb.MongoClient
 // const ObjectID = mongodb.ObjectId
 
-const {MongoClient, ObjectID} = require('mongodb') // object destructing
+const {MongoClient, ObjectID, ObjectId} = require('mongodb') // object destructering
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
 
-const id = new ObjectID()
-console.log(id.id.length);
+// const id = new ObjectID()
+// console.log(id.id.length);
 
 MongoClient.connect(connectionURL, { useNewURLParser : true}, (error, client)=>{
     if (error){
@@ -51,13 +51,16 @@ MongoClient.connect(connectionURL, { useNewURLParser : true}, (error, client)=>{
 
     // db.collection('tasks').insertMany([
     //     {
-    //         'Breakfast completed' : true
+    //         Description :'Breakfast completed' ,
+    //         completed : true
     //     },
     //     {
-    //         'Lunch completed' : false
+    //         Description:'Lunch completed' ,
+    //         completed: false
     //     },
     //     {
-    //         'Dinner Completed' : true
+    //         Description:'Dinner Completed',
+    //         completed : true
     //     }
     // ], (error, result) => {
     //     if (error) {
@@ -66,4 +69,65 @@ MongoClient.connect(connectionURL, { useNewURLParser : true}, (error, client)=>{
 
     //     console.log(result);
     // })
+
+    // db.collection('users').findOne({name : 'Amit'}, (error, user) =>{
+    //     if (error){
+    //         return console.log(error);
+    //     }
+    //     console.log(user);
+    // })
+
+    // db.collection('users').find({age : 26}).toArray((error, users) =>{
+    //     console.log(users);
+    // })
+
+    // db.collection('tasks').findOne({_id : new ObjectID("62dec9307b6c769ec1d039be")}, (error, user) =>{
+    //         if (error){
+    //             return console.log(error);
+    //         }
+    //         console.log(user);
+    //     })
+    
+    
+    // db.collection('tasks').find({completed : false}).toArray((error, users) =>{
+    //     console.log(users);
+    // })
+
+    // db.collection('users').updateOne({_id : new ObjectId('62dc281b99fa6694f0c1403c')}, 
+    //     {
+    //     //     $set : {
+    //     //         name : 'Amit Kumar'
+    //     // }
+    //      $inc: { age: 5 } 
+    // }).then((result) =>{
+    //     console.log(result);
+    // }).catch((error) =>{
+    //     console.log(error);
+    // })
+
+    // db.collection('tasks').updateMany({}, {
+    //     $set : {
+    //         completed : true
+    //     }
+    // }).then((result) =>{
+    //         console.log(result);
+    //     }).catch((error) =>{
+    //         console.log(error);
+    //     })
+
+    // db.collection('users').deleteMany({
+    //     name : 'Amit'
+    // }).then((result) =>{
+    //     console.log(result);
+    // }).catch((error) =>{
+    //     console.log(error);
+    // })
+
+    db.collection('tasks').deleteOne({
+        Description : 'Lunch completed'
+    }).then((result) =>{
+        console.log(result);
+    }).catch((error)=>{
+        console.log(error);
+    })
 })
